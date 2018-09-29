a9ff02b69193a2fb5bceb98b5d9b4d234042f49d
Joseph Lin <joseph.lin@aliyun.com>
2018-09-29 16:22:58 +0800

    教程2 - 数据库配置/安装到 Django & 创建模型

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+	"""问题模型
+	
+	包含了： 1. 问题描述；	2. 发布日期
+	
+	Extends:
+		models.Model
+	
+	Variables:
+		question_test {string} -- 问题描述
+		pub_date {date} -- 发布日期
+	"""
+	question_text = models.CharField( max_length=200 )
+	pub_date      = models.DateTimeField('date published')
+
+
+
+
+class Choice(models.Model):
+	"""choice 选择模型
+	包含了： 1. 选项描述； 2. 当前的票数
+	"""
+	question    = models.ForeignKey(Question, on_delete=models.CASCADE)
+	choice_text = models.CharField(max_length=200)
+	votes		= models.IntegerField(default=0) 
+
+	def __init__(self, arg=None):
+		self.arg = arg
+		
+
