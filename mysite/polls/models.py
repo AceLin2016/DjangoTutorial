0825b6c59a3f463b71e1ad7fe61143411cc04573
Joseph Lin <joseph.lin@aliyun.com>
2018-09-29 17:45:40 +0800

    教程2 - 添加打印消息

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
 # Create your models here.
-class Question(models.Model):
+
+from .AttrDisplay import AttrDisplay
+from django.utils import timezone
+import datetime
+class Question(models.Model, AttrDisplay):
 	"""问题模型
 	
 	包含了： 1. 问题描述；	2. 发布日期
@@ -10,16 +14,20 @@ class Question(models.Model):
 		models.Model
 	
 	Variables:
-		question_test {string} -- 问题描述
+		question_text {string} -- 问题描述
 		pub_date {date} -- 发布日期
 	"""
 	question_text = models.CharField( max_length=200 )
 	pub_date      = models.DateTimeField('date published')
 
+	def __str__(self):
+		return self.question_text
+
+	def was_published_recently(self):
+		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
-
-class Choice(models.Model):
+class Choice(models.Model, AttrDisplay):
 	"""choice 选择模型
 	包含了： 1. 选项描述； 2. 当前的票数
 	"""
@@ -30,4 +38,6 @@ class Choice(models.Model):
 	def __init__(self, arg=None):
 		self.arg = arg
 		
+	def __str__(self):
+		return self.choice_text
 
