691316be9ba75edd4282bb268b8783b41ac8af83
Joseph Lin <joseph.lin@aliyun.com>
2018-09-30 16:32:01 +0800

    教程3 - 一个快捷函数： get_object_or_404()  尝试用 get() 函数获取一个对象，如果不存在就抛出 Http404 错误也是一个普遍的流程。  Django 也提供了一个快捷函数 í ½í±

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -21,12 +21,9 @@ def index(request):
     return render( request, 'polls/index.html', context)
 
 from django.http import Http404
+from django.shortcuts import get_object_or_404
 def detail(request, question_id):
-	try:
-		question = Question.objects.get(pk=question_id)
-
-	except Question.DoesNotExist:
-		raise Http404("Question does not exist")
+	question = get_object_or_404(Question, pk=question_id)
 	return render (request, 'polls/detail.html', {'question':question,} )
 
 
